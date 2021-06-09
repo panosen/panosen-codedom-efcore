65d274781811cde8ab048e4c8e6ced207d7abae2
hc_zhang <hc_zhang@trip.com>
2021-06-09 14:51:25 +0800

    update *.bat

--- 0 - Build-Publish-Move.bat
+++ 0 - Build-Publish-Move.bat
@@ -4,9 +4,6 @@ dotnet restore
 
 dotnet build --no-restore -c Release
 
-dotnet nuget push Panosen.CodeDom.EFCore\bin\Release\Panosen.CodeDom.EFCore.*.nupkg -s https://package.savory.cn/v3/index.json --skip-duplicate
-dotnet nuget push Panosen.CodeDom.EFCore.Engine\bin\Release\Panosen.CodeDom.EFCore.Engine.*.nupkg -s https://package.savory.cn/v3/index.json --skip-duplicate
-
 move /Y Panosen.CodeDom.EFCore\bin\Release\Panosen.CodeDom.EFCore.*.nupkg D:\LocalSavoryNuget\
 move /Y Panosen.CodeDom.EFCore.Engine\bin\Release\Panosen.CodeDom.EFCore.Engine.*.nupkg D:\LocalSavoryNuget\
 
